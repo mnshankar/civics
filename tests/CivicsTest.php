afcdb198656960cfc6ee4f861fe52ce722722a8c
Shankar Manamalkav <mnshankar@gmail.com>
2016-07-11 16:05:58 -0400

    include questionservice

--- tests/CivicsTest.php
+++ tests/CivicsTest.php
@@ -1,5 +1,5 @@
 <?php
-require '../src/QuestionService.php';
+require __DIR__.'/../src/QuestionService.php';
 use mnshankar\civics\QuestionService;
 
 class CivicsTest extends PHPUnit_Framework_TestCase {
