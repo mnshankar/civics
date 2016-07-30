2348f3b48a0607e3494cc7e32f8556a875b3c927
Shankar Manamalkav <mnshankar@users.noreply.github.com>
2016-07-30 13:59:28 -0400

    Update routes.php
    remove web middleware for laravel 5.1

--- src/routes.php
+++ src/routes.php
@@ -1,13 +1,10 @@
 <?php
-//require use of web middleware since we use sessions
-Route::group(['middleware' => ['web']], function () {
-    
-    Route::get('/civics/shuffle', function () {
-        \Session::remove('qArray');
-        return redirect()->route('question.show');
-    });
 
-    Route::get('/civics/{id?}', array('as' => 'question.show', 'uses' => 'mnshankar\civics\QuestionController@show'));
-    Route::post('/civics/{id}/update', array('as' => 'question.update', 'uses' => 'mnshankar\civics\QuestionController@update'));
-
+Route::get('/civics/shuffle', function () {
+    \Session::remove('qArray');
+    return redirect()->route('question.show');
 });
+
+Route::get('/civics/{id?}', array('as' => 'question.show', 'uses' => 'mnshankar\civics\QuestionController@show'));
+Route::post('/civics/{id}/update', array('as' => 'question.update', 'uses' => 'mnshankar\civics\QuestionController@update'));
+
