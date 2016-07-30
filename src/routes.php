72dc47da1bf9fbc14bb36ba24bded2856e5baab5
Shankar Manamalkav <mnshankar@users.noreply.github.com>
2016-07-30 14:07:04 -0400

    Update routes.php

--- src/routes.php
+++ src/routes.php
@@ -1,10 +1,11 @@
 <?php
-
-Route::get('/civics/shuffle', function () {
-    \Session::remove('qArray');
-    return redirect()->route('question.show');
+//require use of web middleware since we use sessions
+Route::group(['middleware' => ['web']], function () {
+    Route::get('/civics/shuffle', function () {
+        \Session::remove('qArray');
+        return redirect()->route('question.show');
+    });
+    
+    Route::get('/civics/{id?}', array('as' => 'question.show', 'uses' => 'mnshankar\civics\QuestionController@show'));
+    Route::post('/civics/{id}/update', array('as' => 'question.update', 'uses' => 'mnshankar\civics\QuestionController@update'));
 });
-
-Route::get('/civics/{id?}', array('as' => 'question.show', 'uses' => 'mnshankar\civics\QuestionController@show'));
-Route::post('/civics/{id}/update', array('as' => 'question.update', 'uses' => 'mnshankar\civics\QuestionController@update'));
-
